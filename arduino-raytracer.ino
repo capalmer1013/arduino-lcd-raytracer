18a5751ee067d5e79907c64d894f2c36c8082862
Chris Palmer <capalmer1013@gmail.com>
2022-08-23 01:32:36 -0400

    2 spheres
    diverting from the tutorial for convenience of things working over being easier to add things to later

--- arduino-raytracer.ino
+++ arduino-raytracer.ino
@@ -32,6 +32,12 @@ color ray_color(const ray& r) {
     vec3 N = unit_vector(r.at(t) - vec3(0,0,-1));
     return 0.5*color(N.x()+1, N.y()+1, N.z()+1);
   }
+  t = hit_sphere(point3(0,-100.5,-1), 100, r);
+  if (t > 0.0) {
+    vec3 N = unit_vector(r.at(t) - vec3(0,0,-1));
+    return color(0.0, 1.0, 0.0);
+  }
+  
   vec3 unit_direction = unit_vector(r.direction());
   t = 0.5*(unit_direction.y() + 1.0);
   return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
